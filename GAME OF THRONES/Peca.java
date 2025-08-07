315542e34ca1ef0676991782f280a665fbe28d1a
Vitória Martins <133159073+queenvickyyyyyyyyyyyyyyyyyyyy@users.noreply.github.com>
2025-08-06 23:23:29 -0300

    Update Peca.java

---
+++ GAME OF THRONES/Peca.java
@@ -0,0 +1,43 @@
+public abstract class Peca{
+    private String nome;
+    private String simbolo;
+    protected int posicaoX;
+    protected int posicaoY;
+    private jogador jogadorDono;
+
+    public Peca(String nome, String simbolo, int posicaoX, int posicaoY, boolean jogadorDono){
+        this.nome = nome;
+        this.simbolo = simbolo;
+        this.posicaoX = posicaoX;
+        this.posicaoY = posicaoY;
+        this.jogadorDono =jogadorDono;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getSimbolo(){
+        return simbolo;
+    }
+
+    public int getPosicaoX(){
+        return posicaoX;
+    }
+
+    public int getPosicaoY(){
+        return posicaoY;
+    }
+    
+    public jogador getJogadorDono(){
+        return jogadorDono;
+    }
+
+    public abstract boolean podeMoverPara(int novoX, int novoY);
+
+    public void moverPara(int novoX, int novoY){
+        this.posicaoX = novoX;
+        this.posicaoY = novoY;
+    }
+
+}
